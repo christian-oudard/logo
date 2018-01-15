4f4dfe91919bbe3ca07df160c62cbd5b20992c55
Christian Oudard <christian.oudard@gmail.com>
2018-01-15 09:40:07 -0800

    Minor cleanup.

--- crossovers.py
+++ crossovers.py
@@ -7,7 +7,7 @@ tau = 2 * sy.pi
 # For even i in i*(tau/14), the intersection is at radius 11/6,
 # and for odd i, the intersection is at radius 17/6.
 def f(theta):
-    return sy.Rational(7,3) + sy.cos(sy.Rational(7,3) * theta)
+    return sy.Rational(7, 3) + sy.cos(sy.Rational(7, 3) * theta)
 
 def calculate_crossovers():
     points = set()
@@ -21,9 +21,9 @@ def calculate_crossovers():
     for i in range(14):
         theta = tau * i/14
         if i % 2 == 0:
-            r = sy.Rational(11,6)
+            r = sy.Rational(11, 6)
         else:
-            r = sy.Rational(17,6)
+            r = sy.Rational(17, 6)
         assert (r, theta) in points
         crossovers.append((r, theta))
 
